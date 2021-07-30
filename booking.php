7b543df8037bc3ede85f934d4353d275f9b39f3e
krishdesai62 <krish22desai@gmail.com>
2021-07-30 11:10:20 +0530

    spelling mistakes resolved. almost done

--- booking.php
+++ booking.php
@@ -78,7 +78,7 @@ if(!isset($_SESSION['login_customer'])){
                         <a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_customer']; ?></a>
                     </li>
                     <ul class="nav navbar-nav">
-            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Garagge <span class="caret"></span> </a>
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Garage <span class="caret"></span> </a>
                 <ul class="dropdown-menu">
               <li> <a href="prereturncar.php">Return Now</a></li>
               <li> <a href="mybookings.php"> My Bookings</a></li>
